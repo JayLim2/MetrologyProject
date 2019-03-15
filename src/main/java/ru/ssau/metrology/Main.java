7237e2615d5f52950493e1077e292622ca45d9ab
Egorpom <Egorpom@yandex.ru>
2019-03-15 13:35:35 +0400

    init commit

---
+++ src/main/java/ru/ssau/metrology/Main.java
@@ -0,0 +1,22 @@
+package ru.ssau.metrology;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main  extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Main.class.getResource("/fxml/Main.fxml"));
+        primaryStage.setTitle("Метрология");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/ru/ssau/metrology/MainController.java
@@ -0,0 +1,4 @@
+package ru.ssau.metrology;
+
+public class MainController {
+}
