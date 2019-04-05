e384ed7fadfdb15352cbbbc693ded9fff7ba1e69
Egorpom <Egorpom@yandex.ru>
2019-04-05 09:13:38 +0400

    End refactor, broke logic

--- src/main/java/ru/ssau/metrology/controllers/MainController.java
+++ src/main/java/ru/ssau/metrology/controllers/MainController.java
@@ -1,30 +1,41 @@
 package ru.ssau.metrology.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import ru.ssau.metrology.models.Distribution;
 import ru.ssau.metrology.models.Validator;
-import ru.ssau.metrology.models.toTable;
 import ru.ssau.metrology.view.TablePoint;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class MainController {
+    private static final Double[] ROMANOV_RATE = {0.01, 0.02, 0.05, 0.10};
+    private static final double[][] romanovTable = {
+            {1.73, 1.72, 1.71, 1.69},
+            {2.16, 2.13, 2.10, 2.00},
+            {2.43, 2.37, 2.27, 2.17},
+            {2.62, 2.54, 2.41, 2.29},
+            {2.75, 2.66, 2.52, 2.39},
+            {2.90, 2.80, 2.64, 2.49},
+            {3.08, 2.96, 2.78, 2.62}
+    };
 
-    public TableColumn pointTable;
-    public TableView<toTable> tableArray;
-    public TableColumn tableBoolean;
-    public TextArea textArea;
-    public TextArea textAreaAllow;
-    public RadioButton sigmMetod;
-    public RadioButton romanovMetod;
-    public RadioButton shevineMetod;
+    private enum Criteria {
+        NONE, SIGMA, ROMANOV, CHAUVENET
+
+    }
+
+    public RadioButton radioSigmaCriteria;
+    public RadioButton radioRomanovCriteria;
+    public RadioButton radioChauvenetCriteria;
     public TextField addText;
-    public TextField romanovParametr;
-    public TableView<TablePoint> tableOfValues;
+    public ComboBox<Double> comboRomanovRate;
+    public TableView<TablePoint> tableOfPoints;
     private Validator validator;
 
     @FXML
@@ -37,179 +48,169 @@ public class MainController {
     private ComboBox<Distribution> distributionsComboBox;
 
     public void initialize() {
-        inputArray = new ArrayList<toTable>();
+        comboRomanovRate.getItems().addAll(Arrays.asList(ROMANOV_RATE));
+        comboRomanovRate.getSelectionModel().selectFirst();
 //        ObservableList<Distribution> distributions = FXCollections.observableList(Arrays.asList(Distribution.values()));
         //       distributionsComboBox.setItems(distributions);
         //      distributionsComboBox.getSelectionModel().selectFirst();
 
     }
 
-    List<toTable> inputArray;
-
     public void onCalculateAction() {
         if (valuesCountInput.getText() != null) {
             int coutnInputValues = Integer.parseInt(valuesCountInput.getText());
             if ((rangeFromInput.getText() != null) && (rangeToInput.getText() != null)) {
-                textArea.clear();
-                textAreaAllow.clear();
                 double rangeFromInputDouble = Double.parseDouble(rangeFromInput.getText());
                 double rangeToInputDouble = Double.parseDouble(rangeToInput.getText());
-                inputArray.clear();
+                tableOfPoints.getItems().clear();
                 double step = (rangeToInputDouble - rangeFromInputDouble) / coutnInputValues;
                 Random r = new Random();
                 for (int i = 0; i < coutnInputValues; i++) {
                     double mx = rangeFromInputDouble + i * step + step / 2;
                     double dx = step / 2;
-                    // inputArray.add(new toTable(((r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() - 3) / Math.sqrt(0.25)) * dx + mx));
-                    // textArea.insertText(0,String.valueOf(((r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() - 3) / Math.sqrt(0.25)) * dx + mx)+"\n");
-                    //textArea.insertText(0,String.valueOf((r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian())/6*dx+mx)+"\n");
-                    inputArray.add(new toTable(((r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian() + r.nextGaussian()) / 6 * dx + mx)));
-                    // inputArray.add(new toTable(((r.nextDouble()*dx+mx))));
-                    textArea.insertText(0, String.valueOf(inputArray.get(inputArray.size() - 1).getPoint()) + "\n");
-                    textAreaAllow.insertText(0, inputArray.get(inputArray.size() - 1).getAllow() + "\n");
+                    tableOfPoints.getItems().add(new TablePoint(((r.nextDouble() + r.nextDouble() + r.nextDouble() + r.nextDouble() + r.nextDouble() + r.nextDouble()) / 6 * dx + mx)));
                 }
-                // pointTable.getColumns().add(inputArray);
-            }
-
-        }
-    }
-
-    public void onWork(ActionEvent actionEvent) {
-        //textArea.clear();
-        textAreaAllow.clear();
-        if (sigmMetod.isSelected()) {
-            for (toTable t : inputArray) {
-                double mx = 0;//матожидание
-                double sigma = 0;
-                for (toTable t2 : inputArray) {
-                    if (t != t2) mx += t2.getPoint();
-                }
-                for (toTable t2 : inputArray) {
-                    if (t != t2) sigma += Math.pow(t2.getPoint() - mx, 2);
-                }
-                sigma = Math.sqrt(sigma / (inputArray.size() - 1));
-                if (Math.abs(mx - t.getPoint()) > 3 * sigma) t.setAllow("не подходит");
-                else t.setAllow("подходит");
-                //textArea.insertText(0,String.valueOf(t.getPoint())+"\n");
-                textAreaAllow.insertText(0, String.valueOf(t.getAllow()) + "\n");
-            }
-
-        }
-        if (romanovMetod.isSelected()) {
-
-            Double u = (Double.valueOf(romanovParametr.getText()) * 100);//тут на самом деле значение 0.01,0.02,0.05 или 0.1 вводимые пользователем на форме умноженные на 100(?) или витсчкейс перепишите
-            int p = u.intValue();
-            double[][] tableRomavski = {{1.73, 1.72, 1.71, 1.69},
-                    {2.16, 2.13, 2.10, 2.00},
-                    {2.43, 2.37, 2.27, 2.17},
-                    {2.62, 2.54, 2.41, 2.29},
-                    {2.75, 2.66, 2.52, 2.39},
-                    {2.90, 2.80, 2.64, 2.49},
-                    {3.08, 2.96, 2.78, 2.62}};
-            for (toTable t : inputArray) {
-                double mx = 0;//матожидание
-                double sigma = 0;
-                for (toTable t2 : inputArray) {
-                    if (t2 != t) mx += t2.getPoint();
-                }
-                for (toTable t2 : inputArray) {
-                    if (t2 != t) sigma += Math.pow(t2.getPoint() - mx, 2);
-                }
-                sigma = Math.sqrt(sigma / (inputArray.size() - 1));
-                int size = inputArray.size();
-                double b = 0;
-                switch (p) { //Тут я не уверена что в проверке именно меньше, потому что в таблице там равно именно, но тогда мы тип не то количество от пользователя отбарсываем или что?
-                    case (1): {
-                        if (size < 4) b = tableRomavski[0][0];
-                        else if (size < 6) b = tableRomavski[1][0];
-                        else if (size < 8) b = tableRomavski[2][0];
-                        else if (size < 10) b = tableRomavski[3][0];
-                        else if (size < 12) b = tableRomavski[4][0];
-                        else if (size < 15) b = tableRomavski[5][0];
-                        else b = tableRomavski[6][0];
-                    }
-                    case (2): {
-                        if (size < 4) b = tableRomavski[0][1];
-                        else if (size < 6) b = tableRomavski[1][1];
-                        else if (size < 8) b = tableRomavski[2][1];
-                        else if (size < 10) b = tableRomavski[3][1];
-                        else if (size < 12) b = tableRomavski[4][1];
-                        else if (size < 15) b = tableRomavski[5][1];
-                        else b = tableRomavski[6][1];
-                    }
-                    case (5): {
-                        if (size < 4) b = tableRomavski[0][2];
-                        else if (size < 6) b = tableRomavski[1][2];
-                        else if (size < 8) b = tableRomavski[2][2];
-                        else if (size < 10) b = tableRomavski[3][2];
-                        else if (size < 12) b = tableRomavski[4][2];
-                        else if (size < 15) b = tableRomavski[5][2];
-                        else b = tableRomavski[6][2];
-                    }
-                    case (10): {
-                        if (size < 4) b = tableRomavski[0][3];
-                        else if (size < 6) b = tableRomavski[1][3];
-                        else if (size < 8) b = tableRomavski[2][3];
-                        else if (size < 10) b = tableRomavski[3][3];
-                        else if (size < 12) b = tableRomavski[4][3];
-                        else if (size < 15) b = tableRomavski[5][3];
-                        else b = tableRomavski[6][3];
-                    }
-                }
-                if ((Math.abs(mx - t.getPoint()) / sigma) > b) t.setAllow("не подходит");
-                else t.setAllow("подходит");
-                textAreaAllow.insertText(0, String.valueOf(t.getAllow()) + "\n");
-            }
-
-
-        }
-        if (shevineMetod.isSelected()) {
-            for (toTable t : inputArray) {
-                double mx = 0;//матожидание
-                double sigma = 0;
-                for (toTable t2 : inputArray) {
-                    if (t != t2) mx += t2.getPoint();
-                }
-                for (toTable t2 : inputArray) {
-                    if (t != t2) sigma += Math.pow(t2.getPoint() - mx, 2);
-                }
-                sigma = Math.sqrt(sigma / (inputArray.size() - 1));
-                int size = inputArray.size();
-                if (size <= 4) {
-                    if (Math.abs(mx - t.getPoint()) > 1.6 * sigma) t.setAllow("не подходит");
-                    else t.setAllow("подходит");
-                } else {
-                    if (size <= 6) {
-                        if (Math.abs(mx - t.getPoint()) > 1.7 * sigma) t.setAllow("не подходит");
-                        else t.setAllow("подходит");
-                    } else {
-                        if (size <= 8) {
-                            if (Math.abs(mx - t.getPoint()) > 1.9 * sigma) t.setAllow("не подходит");
-                            else t.setAllow("подходит");
-                        } else {
-                            if (size <= 10) {
-                                if (Math.abs(mx - t.getPoint()) > 2 * sigma) t.setAllow("не подходит");
-                                else t.setAllow("подходит");
-                            }
-
-                        }
-                    }
-                }
-
-                textAreaAllow.insertText(0, String.valueOf(t.getAllow()) + "\n");
             }
         }
     }
 
-    public void toAdd(ActionEvent actionEvent) {
-        inputArray.add(new toTable(Integer.parseInt(addText.getText())));
-        textArea.insertText(0, String.valueOf(inputArray.get(inputArray.size() - 1).getPoint()) + "\n");
-        textAreaAllow.insertText(0, inputArray.get(inputArray.size() - 1).getAllow() + "\n");
+    public void validatePoints() {
+        List<TablePoint> points = tableOfPoints.getItems();
+        Criteria activeCriteria = Criteria.NONE;
+        if (radioSigmaCriteria.isSelected()) {
+            activeCriteria = Criteria.SIGMA;
+        } else if (radioRomanovCriteria.isSelected()) {
+            activeCriteria = Criteria.ROMANOV;
+        } else if (radioChauvenetCriteria.isSelected()) {
+            activeCriteria = Criteria.CHAUVENET;
+        }
+        for (TablePoint point : points) {
+            double mx = calculateMX(points, point);
+            double sigma = calculateSigma(points, point, mx);
+            Boolean isVerified = null;
+            switch (activeCriteria) {
+                case SIGMA:
+                    isVerified = Math.abs(mx - point.getDoubleValue()) <= 3 * sigma;
+                    break;
+                case ROMANOV:
+                    double value = getRomanovTableValue(comboRomanovRate.getValue(), points.size());
+                    isVerified = Math.abs(mx - point.getDoubleValue()) / sigma <= value;
+                    break;
+                case CHAUVENET:
+                    int size = points.size();
+                    if (size <= 4) {
+                        isVerified = Math.abs(mx - point.getDoubleValue()) <= 1.6 * sigma;
+                    } else if (size <= 6) {
+                        isVerified = Math.abs(mx - point.getDoubleValue()) <= 1.7 * sigma;
+                    } else if (size <= 8) {
+                        isVerified = Math.abs(mx - point.getDoubleValue()) <= 1.9 * sigma;
+                    } else if (size <= 10) {
+                        isVerified = Math.abs(mx - point.getDoubleValue()) <= 2 * sigma;
+                    }
+                    break;
+            }
+            point.setVerified(isVerified);
+        }
+        tableOfPoints.refresh();
     }
 
-    public void clear(ActionEvent actionEvent) {
-        inputArray.clear();
-        textArea.clear();
-        textAreaAllow.clear();
+    public void addPoint() {
+        tableOfPoints.getItems().add(new TablePoint(Double.parseDouble(addText.getText())));
+    }
+
+    public void clear() {
+        tableOfPoints.getItems().clear();
+    }
+
+    private double calculateMX(List<TablePoint> points, TablePoint excluded) {
+        double mx = 0.0D;
+        for (TablePoint point : points) {
+            if (point != excluded) {
+                mx += point.getDoubleValue();
+            }
+        }
+        return mx;
+    }
+
+    private double calculateSigma(List<TablePoint> points, TablePoint excluded, double mx) {
+        double sigma = 0.0D;
+        for (TablePoint point : points) {
+            if (point != excluded) {
+                sigma += Math.pow(point.getDoubleValue() - mx, 2);
+            }
+        }
+        sigma = Math.sqrt(sigma / (points.size() - 1));
+        return sigma;
+    }
+
+    private double getRomanovTableValue(double rate, int count) {
+        double value = 0;
+        //Тут я не уверена что в проверке именно меньше, потому что в таблице там равно именно, но тогда мы тип не то количество от пользователя отбарсываем или что?
+        if (rate == 1) {
+            if (count < 4) {
+                value = romanovTable[0][0];
+            } else if (count < 6) {
+                value = romanovTable[1][0];
+            } else if (count < 8) {
+                value = romanovTable[2][0];
+            } else if (count < 10) {
+                value = romanovTable[3][0];
+            } else if (count < 12) {
+                value = romanovTable[4][0];
+            } else if (count < 15) {
+                value = romanovTable[5][0];
+            } else {
+                value = romanovTable[6][0];
+            }
+        } else if (rate == 2) {
+            if (count < 4) {
+                value = romanovTable[0][1];
+            } else if (count < 6) {
+                value = romanovTable[1][1];
+            } else if (count < 8) {
+                value = romanovTable[2][1];
+            } else if (count < 10) {
+                value = romanovTable[3][1];
+            } else if (count < 12) {
+                value = romanovTable[4][1];
+            } else if (count < 15) {
+                value = romanovTable[5][1];
+            } else {
+                value = romanovTable[6][1];
+            }
+        } else if (rate == 5) {
+            if (count < 4) {
+                value = romanovTable[0][2];
+            } else if (count < 6) {
+                value = romanovTable[1][2];
+            } else if (count < 8) {
+                value = romanovTable[2][2];
+            } else if (count < 10) {
+                value = romanovTable[3][2];
+            } else if (count < 12) {
+                value = romanovTable[4][2];
+            } else if (count < 15) {
+                value = romanovTable[5][2];
+            } else {
+                value = romanovTable[6][2];
+            }
+        } else if (rate == 10) {
+            if (count < 4) {
+                value = romanovTable[0][3];
+            } else if (count < 6) {
+                value = romanovTable[1][3];
+            } else if (count < 8) {
+                value = romanovTable[2][3];
+            } else if (count < 10) {
+                value = romanovTable[3][3];
+            } else if (count < 12) {
+                value = romanovTable[4][3];
+            } else if (count < 15) {
+                value = romanovTable[5][3];
+            } else {
+                value = romanovTable[6][3];
+            }
+        }
+        return value;
     }
 }
--- src/main/java/ru/ssau/metrology/view/TablePoint.java
+++ src/main/java/ru/ssau/metrology/view/TablePoint.java
@@ -2,9 +2,13 @@ package ru.ssau.metrology.view;
 
 public class TablePoint {
     private double value;
-    private boolean isVerified;
+    private Boolean isVerified;
 
-    public TablePoint(double value, boolean isVerified) {
+    public TablePoint(double value) {
+        this(value, null);
+    }
+
+    public TablePoint(double value, Boolean isVerified) {
         this.value = value;
         this.isVerified = isVerified;
     }
@@ -13,7 +17,19 @@ public class TablePoint {
         return String.valueOf(value);
     }
 
+    public double getDoubleValue() {
+        return value;
+    }
+
+
     public String getVerified() {
+        if (isVerified == null) {
+            return "Не определено";
+        }
         return isVerified ? "Подходит" : "Не подходит";
+    }
+
+    public void setVerified(Boolean verified) {
+        isVerified = verified;
     }
 }
